47b214a4d59577702d810ebd3f5c63ee918e2d6a
Pankrit Kritkiattisak <pankrit_lnwclub@hotmail.com>
2016-09-19 18:53:16 +0700

    Delete add.php

--- php/add.php
+++
@@ -1,9 +0,0 @@
-<?php
-   	include("connect.php");   	
-   	$link=Connection();
-	$uid1=$POST_["uid"];
-	$query = "INSERT INTO RFID (UID) VALUES ("$uid1")";    	
-   	mysql_query($query,$link);
-	mysql_close($link);
-   	header("Location: index.php");
-?>
