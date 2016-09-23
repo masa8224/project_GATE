5f7ea82f60d47840b42a4adc3a38a64e9bc3be2a
pankrit <pankrit_lnwclub@hotmail.com>
2016-09-23 23:18:15 +0700

    Changes to be committed: 	modified:   php/index.php 	modified:   php/post.php 	modified:   php/style.css
     Untracked files:
    	php/login.php
    	php/nts-logo.png

--- php/index.php
+++ php/index.php
@@ -1,16 +1,36 @@
+<?php 
+	if (!isset($_COOKIE[user])){
+		header("Location: login.php");
+	}	
+?>
 <html>
 <head>
 	<title>RFID LOG</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<h1>Parking lot RFID Identification</h1>
-<hr>
+<body>
+<ul>
+  <li class="name">NTS RFID System</li>  
+  <div style="float: right;">
+  <li class="logout"><a onclick="logout()" href="login.php">logout</a></li>
+  <li><a>Login as <?php echo $_COOKIE[user];?></a></li>  
+  </div>
+</ul>
+
+<div class="content">
+<br>
 <form method="get">
   <table style="width:40%;">
     <tr>
       <th>FILTER > NAME:
       <input class="search" name="filter" type="text" id="filter" value="<?php echo $_GET["filter"];?>">
-      <input type="submit" value="Search"></th>
+      <input type="submit" value="Search">	  
+	  <label class="advance" for="_1">Advance Filter</label>
+	  <input id="_1" type="checkbox"> 
+	  <div class="normal">
+			<input type="date" name="date" value="<?php echo $_GET["date"];?>">
+      </div>
+	  </th>
     </tr>
   </table>
 </form>
@@ -18,7 +38,7 @@
 <?php
 include("connect.php");   	
 $con=Connection();
-if($_GET["filter"] != ""){
+if($_GET["filter"] or $_GET["date"]){
 	$filter=$_GET["filter"];	
 	$query="SELECT RFID.UID,RFID.date,RFID.time,Persons.name,Persons.surname,Persons.reg_date,Persons.SID
 	FROM RFID 
@@ -26,9 +46,11 @@ if($_GET["filter"] != ""){
 	WHERE Persons.name LIKE '%".$filter."%'
 	AND Persons.uid=RFID.UID
 	ORDER BY date DESC, time DESC;";
-	$result = mysqli_query($con,$query);	
+	$result = mysqli_query($con,$query);
 	if (mysqli_num_rows($result)) {
+		$rowcount=mysqli_num_rows($result);		
 		echo "<table>
+		<tr><td class=\"re\" colspan=\"7\">Found ".$rowcount." result</td></tr>
 		<tr>
 		<th>Login DATE</th>
 		<th>Login TIME</th>
@@ -48,10 +70,10 @@ if($_GET["filter"] != ""){
 			echo "<td class=\"center\">" . $row['SID'] . "</td>";
 			echo "<td class=\"center\">" . $row['reg_date'] . "</td>";
 			echo "</tr>";
-		}
+		}		
 		echo "</table>";
 	}else{
-		echo "<h1 class=\"center\">No result</h1>";		
+		echo "<h1 class=\"center\">No result</h1>";				
 	}	
 	mysqli_close($con);
 }else{
@@ -61,7 +83,7 @@ if($_GET["filter"] != ""){
 	ON RFID.UID=Persons.uid
 	ORDER BY date DESC, time DESC;";
 	$result = mysqli_query($con,$query);
-	if (mysqli_num_rows($result)) {
+	if (mysqli_num_rows($result)) {		
 		echo "<table>
 		<tr>
 		<th>Login DATE</th>
@@ -94,5 +116,10 @@ if($_GET["filter"] != ""){
 	mysqli_close($con);
 }
 ?>
-<body>
+<script type="text/javascript">
+function logout(){
+	document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+}
+</script>
+</body>
 </html>
--- php/post.php
+++ php/post.php
@@ -1,14 +1,44 @@
-<?php
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>RFID REGISTRATION</title>
+</head>
+<h1>RFID REGISTRATION FROM</h1>
+<hr>
+<form method="get" action="<?php getsql() ?>">
+    <p>Name</p>
+    <input type="text" name="getsql()">
+    <p>Surname</p>
+    <input type="text" name="surname">
+    <p>SID</p>
+    <input type="number" name="SID">
+	<p>UID</p>
+    <input type="number" name="UID">
+	<p>class</p>
+    <select name="class">
+		<option value="master">MASTER</option>
+		<option value="normal">NORMAL</option>
+	</select>
+    <br>    
+    <input type="submit">
+</form>
+<?php	
 	include("connect.php");   	
-	$link=Connection();
-	$name=$_POST['name'];
-	$surname=$_POST['surname'];
-	$sid=$_POST['SID'];
+	$link=Connection();	
+	$name=$_GET['name'];
+	$surname=$_GET['surname'];
+	$sid=$_GET['SID'];
 	$date=date("Y-m-d");
-	$uid=$_POST['UID'];
-	$class=$_POST['class'];
-	$query = "INSERT INTO `Persons`(`name`, `surname`, `SID`, `reg_date`, `uid`, `class`) VALUES ('$name','	$surname','$sid','$date','$uid','$class')";
-	mysqli_query($link,$query);
+	$uid=$_GET['UID'];
+	$class=$_GET['class'];
+	function getsql(){	
+	if($name and $surname and $sid){			
+		$query = "INSERT INTO `Persons`(`name`, `surname`, `SID`, `reg_date`, `uid`, `class`) VALUES ('$name','$surname','$sid','$date','$uid','$class')";
+		mysqli_query($link,$query);
+	}else{		
+	}
 	mysqli_close($link);
-	header("Location: testform.html");
+	}
 ?>
+<body>
+</html>
