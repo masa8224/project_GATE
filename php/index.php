a752936dcbd834309b601f481706e79701686e9e
Pankrit Kritkiattsak <pankrit_lnwclub@hotmail.com>
2016-10-11 11:41:29 +0700

    modified:   Project_COUNT-2/Project_COUNT-2.ino 	new file:   php/data.xml 	modified:   php/index.php 	modified:   php/login.php 	new file:   php/page.xml 	new file:   php/script.js 	modified:   php/style.css

--- php/index.php
+++ php/index.php
@@ -2,15 +2,17 @@
 	if (!isset($_COOKIE[user])){
 		header("Location: login.php");
 	}	
+	$pageid = "page1";
 ?>
 <html>
 <head>
 	<title>RFID LOG</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	</head>
-<body>
-<ul>
+	<script type="text/javascript" src="script.js"></script>	
+</head>
+<body onload="load()">
+<ul>	
 	<li class="name">NTS RFID System</li> 	
 	<li class="dropdown">
     <a href="#" class="dropbtn">MAIN</a>
@@ -20,15 +22,38 @@
 	  <a href="setting.php">Setting</a>	  
     </div>
 	</li>
-	<li><a>></a></li>
-	<li><a>LOG</a></li>
+	<li><a>></a></li>	
+	<li><a id="pagename"></a></li>	
     <div style="float: right;">
 	<li class="logout"><a href="logout.php">logout</a></li>
 	<li><a>Login as <?php echo $_COOKIE[user];?></a></li>  
 	</div>
+	<?php
+		$xml=simplexml_load_file("page.xml") or die("Error: Cannot create object");		
+		echo "<script>document.getElementById('pagename').innerHTML='".$xml->$pageid."';</script>";
+	?>
 </ul>
-
 <div class="content">
+<div class="tabsheader">
+<ul>
+	<li><a class="tabOn" onclick="opentabs(event,'dashboard');">Dashboard</a></li>
+	<li><a class="tabOn" onclick="opentabs(event,'log');">Log</a></li>	
+</ul>
+</div>
+<div id="dashboard" class="tabs">
+<div style="display: flex;">
+<div class="card animate" style="width: 58%;margin: 1em;display: block;">
+	<font id="count" style="font-size: 50px;"></font>
+	<span style="margin: 1em;">Students was enter today.</span>
+	<hr>
+</div>
+<div class="card animate" style="width: 38%;margin: 1em; padding: 6px;">
+	<div id="chart" class="animate">
+</div>
+</div>
+</div>
+</div>
+<div id="log" class="tabs" style="display:none;">
 <div class="card">
 <table style="width:100%;margin:0 auto 0 auto;border:none;">
 <form method="get">  
@@ -45,10 +70,7 @@
 </form>
 </table>
 </div>
-<div class="card">
-	<div id="chart"></div>
-</div>
-<div class="card cardtable">
+<div class="card cardtable" style="-webkit-animation-timing-function: ease-in-out;">
 <?php
 include("connect.php");   	
 $con=Connection();
@@ -126,60 +148,53 @@ if($_GET["filter"] or $_GET["date"]){
 		echo "</table>";
 	}else{
 		echo "<h1 class=\"center\">No result</h1>";		
-	}	
-	//mysqli_close($con);	
+	}			
 	$query ="SELECT COUNT(1) AS entries, DATE(date) as date FROM RFID GROUP BY DATE(date) LIMIT 0 , 30";
 	$graph = mysqli_query($con,$query);
-	echo "<script type=\"text/javascript\">
-      google.charts.load('current', {'packages':['line']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var data = new google.visualization.DataTable();
-      data.addColumn('date', 'Day');
-      data.addColumn('number', 'Students');
-      data.addRows([";
+	echo "<script type=\"text/javascript\">      
+		var rows=[";
 	while($row = mysqli_fetch_array($graph)){
+		$num = mysqli_num_rows($graph);
+		$rownum = $num - 1;
 		$sqldate = $row['date'];
 		$dateraw = strtotime($sqldate);
 		$date = date("Y m d",$dateraw);
-		echo "[new Date('".$date."'),".$row['entries']."],";
+		echo "[new Date('".$date."'),".$row['entries']."]";
+		if($num >0){
+			echo ",";
+		}
 	}
-    echo "]);
-	var options = {
-        chart: {
-          title: 'Students that use the parking lot',
-        },
-        width: 900,
-        height: 500
-		
-      };
-
-      var chart = new google.charts.Line(document.getElementById('chart'));
-
-      chart.draw(data, options);
-      }
-    </script>";
+    echo "];		
+		drawchart(rows);
+		</script>";
+	$date2 = date("Y-m-d");
+	$query3 ="SELECT COUNT(1) AS entries, DATE(date) as date FROM RFID WHERE date='".$date2."' GROUP BY DATE(date) LIMIT 0 , 30";
+	$stu = mysqli_query($con,$query3);
+	if (mysqli_num_rows($stu)){
+	while($row = mysqli_fetch_array($stu)){
+		$entries = $row['entries'];
+		echo "<script>";
+		echo "document.getElementById('count').innerHTML=".$entries.";";	
+		echo "</script>";
+	}
+	}else{
+		echo "<script>";
+		echo "document.getElementById('count').innerHTML= 0;";
+		echo "</script>";
+	}
+	$week = date("W");
+	mysqli_close($con);
 	
 }
 ?>
 </div>
+</div>
+</div>
 <script type="text/javascript">
-setInterval(Timer(),1000);
-function Timer() {
-   var dt=new Date()
-   var hours = dt.getHours();
-   var min = dt.getMinutes();
-   var sec = dt.getSeconds();
-   if (min < 10) {
-    min = "0" + min;
-   }
-   if (sec<10){
-	sec = "0" + sec;	
-   }
-   document.getElementById('time').innerHTML=hours+":"+min+":"+sec;
-   setTimeout("Timer()",1000);
-}
+		setInterval(Timer(),1000);		
+		function load(){
+			opentabs(event,'dashboard');
+		}
 </script>
 </body>
 </html>
