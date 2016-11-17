1a4632b1273c71b5f849d5011e2b0751063258e7
Pankrit Kritkiattsak <pankrit_lnwclub@hotmail.com>
2016-11-17 16:41:46 +0700

    modified:   gate_Opener/gate_Opener.ino

--- gate_Opener/gate_Opener.ino
+++ gate_Opener/gate_Opener.ino
@@ -1,47 +1,35 @@
-int up = 9;
-int down = 8;
-int upButton = 7;
-int downButton = 6;
-int IR1 = 5;
-int ls1 = 4;
-int ls2 = 3;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(down, OUTPUT);
-  pinMode(up, OUTPUT);
-  pinMode(upButton, INPUT);
-  pinMode(downButton, INPUT);
-  pinMode(IR1, INPUT);
-  pinMode(ls1, INPUT);
-  pinMode(ls2, INPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(6, INPUT);
 }
 
 void loop() {
-  if (digitalRead(4)==HIGH) {
-    //Serial.println("ls up 1");
-    if (digitalRead(IR1)) {
-      if (digitalRead(upButton)) {
-        Serial.println("up!");
-        digitalWrite(up, LOW);
-      } else {
-        digitalWrite(up, HIGH);
-      }
-    } else {
-      digitalWrite(up, LOW);
+  if (digitalRead(4)) {
+    if (analogRead(A0) > 500) {
+      Serial.println("9!");
+      digitalWrite(9, LOW);
+    } else if(!digitalRead(4)) {
+      digitalWrite(9, HIGH);
     }
   } else {
-    digitalWrite(up, LOW);
+    digitalWrite(9, HIGH);
   }
-  //
-  if (digitalRead(3)==HIGH) {
-    //Serial.println("ls down 1");
-    if (digitalRead(downButton)) {
-      Serial.println("down!");
-      digitalWrite(down, LOW);
-    } else {
-      digitalWrite(down, HIGH);
+  
+  if (digitalRead(3)) {
+  if (analogRead(A1) > 500) {
+      Serial.println("8!");
+      digitalWrite(8, LOW);
+  }else if(!digitalRead(3)) {
+      digitalWrite(8, HIGH);
     }
   } else {
-    digitalWrite(down, LOW);
+    digitalWrite(8, HIGH);
   }
+  
+
 }
