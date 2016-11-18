0901e8790f9a7906efdebe941c37099f00648b57
Pankrit Kritkiattsak <pankrit_lnwclub@hotmail.com>
2016-11-18 18:00:59 +0700

    modified:   gate_Opener/gate_Opener.ino

--- gate_Opener/gate_Opener.ino
+++ gate_Opener/gate_Opener.ino
@@ -6,11 +6,12 @@ void setup() {
   pinMode(3, INPUT);
   pinMode(4, INPUT);
   pinMode(6, INPUT);
+  digitalWrite(9,HIGH);
+  digitalWrite(8,HIGH);
 }
-
-void loop() {
+void loop() {/*
   if (digitalRead(4)) {
-    if (analogRead(A0) > 500) {
+    if (analogRead(A1) > 500) {
       Serial.println("9!");
       digitalWrite(9, LOW);
     } else if(!digitalRead(4)) {
@@ -21,7 +22,7 @@ void loop() {
   }
   
   if (digitalRead(3)) {
-  if (analogRead(A1) > 500) {
+  if (analogRead(A0) > 500) {
       Serial.println("8!");
       digitalWrite(8, LOW);
   }else if(!digitalRead(3)) {
@@ -30,6 +31,30 @@ void loop() {
   } else {
     digitalWrite(8, HIGH);
   }
-  
+  */
+  if (!digitalRead(4)){
+    digitalWrite(9,HIGH);
+  }
+  if (!digitalRead(3)){
+    digitalWrite(8,HIGH);
+  }
+  if (analogRead(A0)>500){
+    if (digitalRead(4)){
+      Serial.println("Open");
+      digitalWrite(9,LOW);
+    
+    while (digitalRead(4)==HIGH){}
+      digitalWrite(9,HIGH);    
+    delay(2000);
+    while(digitalRead(6)==LOW){}
+    delay(1000);
+    if (digitalRead(3)){
+      Serial.print("Close");
+      digitalWrite(8,LOW);
+    }
+    while(digitalRead(3)){}
+    digitalWrite(8,HIGH);
+  }
+  }
 
 }
